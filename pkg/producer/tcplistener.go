2dfab85be155c1b2bd6d317a52e9c88b628bfee9
Utkarsh Simha <utkarsh.simha@nutanix.com>
2018-12-19 17:33:04 -0800

    Moved old code of TCP listener creation
    This has to be modified to use a struct and call create on the struct

---
+++ pkg/producer/tcplistener.go
@@ -0,0 +1,46 @@
+package producer
+
+import (
+	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
+	tcp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/tcp_proxy/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/util"
+)
+
+// MakeTCPListener creates a TCP listener for a cluster.
+func MakeTCPListener(listenerName string, port uint32, clusterName string) *v2.Listener {
+	// TCP filter configuration
+	config := &tcp.TcpProxy{
+		StatPrefix: "tcp",
+		ClusterSpecifier: &tcp.TcpProxy_Cluster{
+			Cluster: clusterName,
+		},
+	}
+	pbst, err := util.MessageToStruct(config)
+	if err != nil {
+		panic(err)
+	}
+	return &v2.Listener{
+		Name: listenerName,
+		Address: core.Address{
+			Address: &core.Address_SocketAddress{
+				SocketAddress: &core.SocketAddress{
+					Protocol: core.TCP,
+					Address:  "127.0.0.1",
+					PortSpecifier: &core.SocketAddress_PortValue{
+						PortValue: port,
+					},
+				},
+			},
+		},
+		FilterChains: []listener.FilterChain{{
+			Filters: []listener.Filter{{
+				Name: util.TCPProxy,
+				ConfigType: &listener.Filter_Config{
+					Config: pbst,
+				},
+			}},
+		}},
+	}
+}
